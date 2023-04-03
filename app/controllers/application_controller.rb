509e4d85886b9d38cfc444d58ee2913cb64d4397
yuta10200831 <yfuse20ki@yahoo.co.jp>
2023-04-03 09:30:47 +0900

    ログアウト出来ないの修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,6 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
-
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :name, :password, :profile, :location, :birthday, :profile_image, :header_image])
   end
